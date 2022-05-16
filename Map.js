c62c815edf95c8da3e15143c6378899a7b31c80f
Paul Kluge <kluge.paul@gmx.de>
2022-05-16 15:55:09 +0200

    Map einlesen Koordinaten verbessert

--- Map.js
+++ Map.js
@@ -4,38 +4,52 @@
 class Map {
 
     constructor(roadColor, mapBackground, waypoints, initalEnemyPos) {
-        this.waypoints = [];
+        this.waypoints = waypoints;
         this.mapBackground = mapBackground; 
         this.roadColor = roadColor;
         //this.initalEnemyPos = {x,y}; // Einfügen aus Enemy
     }
 
     draw() {
+        // Canvas erstellen
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
 
+        // Canvas Background
         ctx.fillStyle = this.mapBackground;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        // Vereinfachten Path zeichnen
+        // Waypoints (800,60),(800,200),(200,500)
+        
         ctx.beginPath();
         ctx.strokeStyle = this.roadColor;
+        // Koordinaten Weg
+        let x;
+        let y;
+
+        // Startpunkt weg
         ctx.moveTo(0,60);
-        ctx.lineTo(800, 60);
-        ctx.moveTo(800, 60);
-        ctx.lineTo(800, 200);
-        ctx.moveTo(800, 200);
-        ctx.lineTo(200, 200);
-        ctx.moveTo(200, 500);
-        ctx.lineTo(200, 200);
-        ctx.lineWidth = 1;
+
+        // Weg zeichnen 
+        for (let i = 0; i < 4; i++) {
+            x = this.waypoints[i][0];
+            y = this.waypoints[i][1];
+            ctx.lineTo(x, y);
+        }
+        ctx.lineWidth = 5;
         ctx.stroke();
         ctx.fill();
-        // Waypoints (800,60),(800,200),(200,500)
- 
     }
 }
 
-const map = new Map("#F08080", "#eee");
+const map = new Map("#F08080", 
+                    "#eee", 
+                    [
+                        [800, 60],
+                        [800, 200],
+                        [200, 200],
+                        [200, 500]
+                    ] 
+);
 map.draw();
 console.log(map);
