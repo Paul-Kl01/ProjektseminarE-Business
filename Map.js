19d332cba7a8989f6f539e959f48758d4e5cd06e
Paul Kluge <kluge.paul@gmx.de>
2022-05-18 13:35:13 +0200

    Commit vor Pull Request

--- Map.js
+++ Map.js
@@ -2,55 +2,51 @@
 // import Helper from './Helper';
 
 class Map {
+  constructor(roadColor, mapBackground, waypoints, initalEnemyPos) {
+    this.waypoints = waypoints;
+    this.mapBackground = mapBackground;
+    this.roadColor = roadColor;
+    //this.initalEnemyPos = {x,y}; // Einfügen aus Enemy
+  }
 
-    constructor(roadColor, mapBackground, waypoints, initalEnemyPos) {
-        this.waypoints = waypoints;
-        this.mapBackground = mapBackground; 
-        this.roadColor = roadColor;
-        //this.initalEnemyPos = {x,y}; // Einfügen aus Enemy
+  draw() {
+    // Canvas erstellen (aus Game importieren)
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+
+    // Canvas Background
+    ctx.fillStyle = this.mapBackground;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Waypoints (800,60),(800,200),(200,500)
+
+    ctx.beginPath();
+    ctx.strokeStyle = this.roadColor;
+    // Koordinaten Weg
+    let x;
+    let y;
+
+    // Startpunkt weg
+    ctx.moveTo(0, 60);
+
+    // Weg zeichnen
+    for (let i = 0; i < this.waypoints.length; i++) {
+      x = this.waypoints[i][0];
+      y = this.waypoints[i][1];
+      ctx.lineTo(x, y);
     }
-
-    draw() {
-        // Canvas erstellen
-        var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
-
-        // Canvas Background
-        ctx.fillStyle = this.mapBackground;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-        // Waypoints (800,60),(800,200),(200,500)
-        
-        ctx.beginPath();
-        ctx.strokeStyle = this.roadColor;
-        // Koordinaten Weg
-        let x;
-        let y;
-
-        // Startpunkt weg
-        ctx.moveTo(0,60);
-
-        // Weg zeichnen 
-        for (let i = 0; i < 4; i++) {
-            x = this.waypoints[i][0];
-            y = this.waypoints[i][1];
-            ctx.lineTo(x, y);
-        }
-        ctx.lineWidth = 2;
-        ctx.stroke();
-        ctx.fill();
-    }
+    ctx.lineWidth = 60;
+    ctx.stroke();
+    ctx.fill();
+  }
 }
 
 // Map aufbauen
-const map = new Map("#F08080", 
-                    "#eee", 
-                    [
-                        [800, 60],
-                        [800, 200],
-                        [200, 200],
-                        [200, 500]
-                    ] 
-);
+const map = new Map("#F08080", "#eee", [
+  [800, 60],
+  [800, 200],
+  [200, 200],
+  [200, 500],
+]);
 map.draw();
 console.log(map);
