90f65b2febb5896caf5a757094af91227913fa48
Anika ML <anika.metzger-lindner2@student.uni-halle.de>
2022-05-31 15:11:55 +0200

    Erstellen Game Object Klasse

---
+++ GameObjects.js
@@ -0,0 +1,20 @@
+import Helper from './Helper';
+
+class GameObject {
+    
+constructor(x,y,radius,color){
+    this.x = x; 
+    this.y = y; 
+    this.radius = radius; 
+    this.color = color; 
+//this.objecttype = objecttype // 0 Enemy 1 Tower 2 Particle
+}
+
+create(){}
+
+draw(){
+    Helper.drawCircle(this.x, this.y, this.radius, this.color)
+}
+update(){}
+
+}
