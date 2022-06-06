3e1cc8bc8fa7319e0142c307f0e4c2e97151b1c9
Nicole <82213190+Ni-coleD@users.noreply.github.com>
2022-06-06 13:03:11 +0200

    Update Particle.js

--- js-classes/Particle.js
+++ js-classes/Particle.js
@@ -1,13 +1,18 @@
-// import "./Helper.js"
-// import "./Entities.js"
-// import "./Enemy.js"
-// import "./GameObject.js"
+/*
+ * Partikelerzeugung zum Abschießen der Enemies
+ * @author Nicole
+ *
+ */
+
+//Imports und Instanzerzeugung
 const gameObject = require('./GameObject')
 const enemy = require('./Enemy')
 const helper = require('./Helper')
+const tower = require('./Tower')
 var gameObject_ = new gameObject();
-var enemy_ = new enemy();
+var enemy_;
 var helpers_ = new helper();
+var tower_;
 
 class particle extends gameObject_{
     //eigene Klassen-Referenz auf canvas & context, da auf Game kein Zugriff
@@ -15,12 +20,15 @@ class particle extends gameObject_{
     // ctx = this.canvas.getContext("2d");
 
     constructor (towerX, towerY, closestEnemy) { //Über tower.js aufzurufen
+        //eventuell Referenz auf Tower übergeben und daraus die Koordinaten ziehen, wegen dem Exportieren & Importieren von Klasse?
+        enemy_ = closestEnemy //wahrscheinlich unnötig?
+
         this.x = towerX //Als Startposition Koordinaten des jeweiligen Turms
         this.y = towerY
         this.velocity = {x: 0, y: 0}
         this.color = "#483d8b"
         this.radius = 1
-        this.focusedEnemy = closestEnemy //Referenziert später Enemy die fokussiert werden soll
+        this.focusedEnemy = enemy_ //Referenziert später Enemy der fokussiert werden soll
     }
 
     update() { //Klassenvariablen updaten
@@ -58,7 +66,7 @@ class particle extends gameObject_{
     /*Enemies bewegen sich, Path zum Enemy muss immer wieder aktualisiert werden
      *Bis Enemy getroffen wurde vom Particle
      */
-    calcPathToEnemy() { //Müsste im Rahmen der TowerKlasse nach Konsturktor-Aufruf des Particles einmal inizial aufgerufen werden
+    calcPathToEnemy() { //Müsste im Rahmen der TowerKlasse nach Konsturktor-Aufruf des Particles einmal initial aufgerufen werden
         const angle = Math.atan2(closestEnemy.y - this.y , closestEnemy.x - this.x) //Bestimmt den Winkel zwischen Enemy & Particle
         this.velocity = { //Bestimmt Ratio anhand welcher Particle zum Enemy gepusht wird und speichert dies in der velocity
             x: Math.cos(angle),
